e493ac526a84d34b1fae0d0d08b8f14d8261af1a
yeomim <yeomim@livingroom_pc>
2017-07-17 11:16:11 +0900

    gugudan

---
+++ src/ququdan.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class ququdan {
+	public static void main(String[] args){
+
+		/*
+		System.out.println("구구단 중 출력할 단은?: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("number:"+number);
+		
+		
+		int result=number*1;
+		
+		
+		int i ;
+		i = 10;
+		i =5;
+		
+		
+		final int J;
+		J = 10;
+//		J=5;
+		
+		double circleArea;
+		final double PI = 3.14159;
+		circleArea = 3 * 3 * PI;
+		
+		
+		final int OIL_PRICE = 1450;
+		int totalPrice = 50 * OIL_PRICE;*/
+		
+		
+		System.out.println("구구단 중 출력할 단은?:");
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		System.out.println(num+"단");
+		System.out.println(num*1);
+		System.out.println(num*2);
+		System.out.println(num*3);
+		System.out.println(num*4);
+		System.out.println(num*5);
+		System.out.println(num*6);
+		System.out.println(num*7);
+		System.out.println(num*8);
+		System.out.println(num*9);
+	}
+
+}
+
+
+
+
